dcb3077e6f1c91e6d3fcc15174a033845fd54b99
stas.ivinsky@readsoft.com <stas.ivinsky@readsoft.com>
2015-07-01 11:15:29 +0200

    Basic thresholding is attempted

--- Denoising/denoising_20150701_separate_background.py
+++ Denoising/denoising_20150701_separate_background.py
@@ -4,7 +4,6 @@ from scipy import signal, ndimage
 from PIL import Image
 import gzip
 from sklearn.metrics import mean_squared_error
-import sys
 
 def load(path):
     return np.asarray(Image.open(path))/255.0;
@@ -13,8 +12,26 @@ def save(path, img):
     tmp = np.asarray(img*255.0, dtype=np.uint8);
     Image.fromarray(tmp).save(path);
 
-def discretize(a):
-    return np.uint8((a > 0.40));
+def denoise_im_with_back(inp):
+    # estimate 'background' color by a median filter
+    bg = signal.medfilt2d(inp, 11);
+    save('background.png', bg);
+
+    # compute 'foreground' mask as anything that is significantly darker than
+    # the background
+    mask = inp < bg - 0.1;
+    save('foreground_mask.png', mask);
+    back = np.average(bg);
+    
+    # Lets remove some splattered ink
+    mod = ndimage.filters.median_filter(mask,2);
+    mod = ndimage.grey_closing(mod, size=(2,2));
+       
+    # either return forground or average of background
+       
+    out = np.where(mod, inp, back);  ## 1 is pure white    
+    return out;
+
 
 
 # Main program starts here!
@@ -38,7 +55,7 @@ for f in os.listdir(train_path):
     facitimage = load(facit_path+"\\"+f);
     trainimage = load(train_path+"\\"+f);
 
-    newimage = discretize(trainimage);
+    newimage = denoise_im_with_back(trainimage);
 
     facitimage_all = np.concatenate((facitimage_all, facitimage.reshape(facitimage.shape[0] * facitimage.shape[1])));
     newimage_all = np.concatenate((newimage_all, newimage.reshape(newimage.shape[0] * newimage.shape[1])));
@@ -48,9 +65,6 @@ RMSE = mean_squared_error(newimage_all, facitimage_all)**0.5;
 print ('Training is completed. ');
 print (RMSE);
 
-# Training result is 0.136614401653
-
-#sys.exit("Break!")
 
 # Perform calculation on the test data and produce new set of images
 
@@ -62,7 +76,7 @@ for f in os.listdir(out_path):
 for f in os.listdir(test_path):
     testimage = load(test_path+"\\"+f);
 
-    newimage = discretize(testimage);
+    newimage = denoise_im_with_back(testimage);
 
     save(out_path+"\\"+f, newimage);
 
@@ -87,6 +101,4 @@ for f in os.listdir(test_path):
 submission.close();
 
 print ('Preparation of submission file is completed');
-
-# Submission result is 0.13066
 
